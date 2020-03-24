ecbdf7dbb0a709d90ed32c699fb0b43e56354b11
Amina55555 <57752741+Amina55555@users.noreply.github.com>
2020-03-24 20:26:50 +0100

    2éme commit

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
         $repo =$this->getDoctrine()->getRepository(Article::class);
 
@@ -33,17 +42,59 @@ class BlogController extends AbstractController
 
         ]);
     }
+
+     /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+       public function form(Article $article = null, Request $request, ObjectManager $manager){
+
+           if(!$article) { 
+                $article = new Article();   
+           }
+       
+
+        //    $article->setTitle("Titre d'exemple")
+        //             ->setContent("Le contenu de l'article");
+
+        //    $form = $this->createFormBuilder($article)
+        //    ->add('title')
+        //    ->add('content')
+        //    ->add('image')
+        //    ->getForm();
+        $form = $this->createForm(ArticleType::class, $article);
+
+           $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()) {
+        if(!$article->getId()){
+            $article->setCreatedAt(new \DateTime());
+        }
+
+        
+
+        $manager->persist($article);
+        $manager->flush();
+
+        return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+    }
+         
+           return $this->render('blog/create.html.twig',[
+               'formArticle' => $form ->createView(),
+               'editMode' => $article->getId() !== null
+           ]);
+         
+        }
+    
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show($id){
-        $repo =$this->getDoctrine()->getRepository(Article::class);
-
-        $article = $repo->find($id);
-
-        return $this->render('blog/show.html.twig',[
+    public function show( Article $article){
+      
+      return $this->render('blog/show.html.twig',[
            'article'=> $article
         ]);
     
    }
+    
 }
